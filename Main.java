ef64346323c43a907b5ad8b3467d40b4c405f5f6
COSTE Julie <julieanne.coste@gmail.com>
2024-04-05 20:00:10 +0200

    creation class tank

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // C'est une idée pas dutout définitive pour le main
+
+        // Create a new water tank with a water level of 50.00
+        Tank tank = new Tank(100.00);
+        // Create a new producer
+        // Producer producer = new Producer(tank);
+        // Create a new consumer
+        // Consumer consumer = new Consumer(tank);
+        // Start the producer and consumer threads
+        // producer.start();
+        // consumer.start();
+    }
+}
--- Producer.java
+++ Producer.java
@@ -1,3 +1,6 @@
 public class Producer {
 
+    public Producer() {
+    }
+
 }
